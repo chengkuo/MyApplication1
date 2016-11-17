129cb5b8e8e9f7ac9d07e791bd67b3f97a9981b9
chengkuo <597863395@qq.com>
2016-11-17 22:07:04 +0800

    new chatroom

--- app/src/main/java/ChatRoomHelper.java
+++ app/src/main/java/ChatRoomHelper.java
@@ -61,7 +61,6 @@ public class ChatRoomHelper {
 
 
 
-
     private Map<String, Map<String, ChatRoomMember>> cache = new HashMap<>();
 
     private List<RoomMemberChangedObserver> roomMemberChangedObservers = new ArrayList<>();
