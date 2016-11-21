f48452cb276d4c0ab89f6d1a0365b2098ef4f994
chengkuo <597863395@qq.com>
2016-11-21 20:56:12 +0800

    聊天室列表

--- app/src/main/java/com/example/a52374/myapplication/fragment/Live.java
+++ app/src/main/java/com/example/a52374/myapplication/fragment/Live.java
@@ -1,6 +1,7 @@
 package com.example.a52374.myapplication.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,11 +9,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
 import com.example.a52374.myapplication.R;
 import com.example.a52374.myapplication.adapter.ChatRoomAdapter;
+import com.example.a52374.myapplication.avtivity.ChatRoomActivity;
+import com.example.a52374.myapplication.avtivity.PersondataActivity;
 import com.example.a52374.myapplication.mybean.ChatRoomHttpClient;
 import com.netease.nimlib.sdk.chatroom.model.ChatRoomInfo;
 
@@ -52,18 +56,44 @@ public class Live extends Fragment {
     private static final String RESULT_KEY_BROADCAST_URL = "broadcasturl";
     private static final String RESULT_KEY_ONLINE_USER_COUNT = "onlineusercount";
 
+    private final static String EXTRA_ROOM_ID = "ROOM_ID";
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_chat_room, container, false);
+        return inflater.inflate(R.layout.chat_rooms_gridview, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initView(view);
-        ChatRoomHttpClient.getInstance().fetchChatRoomList(new ChatRoomHttpClient.ChatRoomHttpCallback<List<ChatRoomInfo>>() {
+        initAdpter(items, getContext());
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getContext(),ChatRoomActivity.class);
+                intent.putExtra(EXTRA_ROOM_ID,items.get(position).getRoomId());
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private void initAdpter(List<ChatRoomInfo> items, Context context) {
+        myAdpter = new ChatRoomAdapter(items, context);
+        gridView.setAdapter(myAdpter);
+    }
+
+
+    private void initView(View view) {
+        gridView = (GridView) view.findViewById(R.id.chat_room_gridview);
+
+        //获取聊天室
+        ChatRoomHttpClient.getInstance().fetchChatRoomList(
+                new ChatRoomHttpClient.ChatRoomHttpCallback<List<ChatRoomInfo>>() {
             @Override
             public void onSuccess(List<ChatRoomInfo> rooms) {
                 if (items.isEmpty()) {
@@ -77,28 +107,14 @@ public class Live extends Fragment {
             public void onFailed(int code, String errorMsg) {
                 onFetchDataDone(false);
                 if (getActivity() != null) {
-                    Toast.makeText(getActivity(), "fetch chat room list failed, code=" + code, Toast.LENGTH_SHORT);
+                    Toast.makeText(getActivity(), "fetch chat room list failed, code="
+                            + code, Toast.LENGTH_SHORT);
                 }
 
                 Log.d("cmd", "fetch chat room list failed, code:" + code
                         + " errorMsg:" + errorMsg);
             }
         });
-
-        initAdpter(items, getContext());
-
-    }
-
-    private void initAdpter(List<ChatRoomInfo> items, Context context) {
-        myAdpter = new ChatRoomAdapter(items, context);
-
-        gridView.setAdapter(myAdpter);
-    }
-
-
-    private void initView(View view) {
-        gridView = (GridView) view.findViewById(R.id.chat_room_gridview);
-
     }
     private void onFetchDataDone(boolean success) {
 
@@ -114,68 +130,3 @@ public class Live extends Fragment {
     }
 
 }
-/*
-//将map转换成josn
-class ExtensionHelper{
-    public static Map<String, Object> getMapFromJsonString(final String jsonStr) {
-        if (TextUtils.isEmpty(jsonStr)) {
-            return null;
-        }
-
-        try {
-            org.json.JSONObject json = new org.json.JSONObject(jsonStr);
-            return recursiveParseJsonObject(json);
-        } catch (org.json.JSONException e) {
-            Log.e("tmd", "getMapFromJsonString exception =" + e.getMessage());
-        }
-
-        return null;
-    }
-
-    private static Map<String, Object> recursiveParseJsonObject(org.json.JSONObject json) throws org.json.JSONException {
-        if (json == null) {
-            return null;
-        }
-
-        Map<String, Object> map = new HashMap<>(json.length());
-        String key;
-        Object value;
-        Iterator<String> i = json.keys();
-        while (i.hasNext()) {
-            key = i.next();
-            value = json.get(key);
-            if (value instanceof org.json.JSONArray) {
-                map.put(key, recursiveParseJsonArray((org.json.JSONArray) value));
-            } else if (value instanceof org.json.JSONObject) {
-                map.put(key, recursiveParseJsonObject((org.json.JSONObject) value));
-            } else {
-                map.put(key, value);
-            }
-        }
-
-        return map;
-    }
-
-
-    private static List recursiveParseJsonArray(org.json.JSONArray array) throws org.json.JSONException {
-        if (array == null) {
-            return null;
-        }
-
-        List list = new ArrayList(array.length());
-        Object value;
-        for (int m = 0; m < array.length(); m++) {
-            value = array.get(m);
-            if (value instanceof org.json.JSONArray) {
-                list.add(recursiveParseJsonArray((org.json.JSONArray) value));
-            } else if (value instanceof org.json.JSONObject) {
-                list.add(recursiveParseJsonObject((org.json.JSONObject) value));
-            } else {
-                list.add(value);
-            }
-        }
-
-        return list;
-    }
-
-}*/
